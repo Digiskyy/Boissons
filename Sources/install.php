360a5ed08994af7feaf5079c45819d2f68af66ad
Digiskyy <a-blaise@orange.fr>
2019-11-25 23:58:34 +0100

    Table Constitution well-filled => All database is ok

--- Sources/install.php
+++ Sources/install.php
@@ -13,7 +13,7 @@
 
 					CREATE TABLE Recettes (
 						idRecette INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-						titre VARCHAR(100) NOT NULL,
+						titre VARCHAR(100) NOT NULL UNIQUE,
 						composition TEXT NOT NULL,
 						preparation TEXT NOT NULL
 					) ;
@@ -35,8 +35,8 @@
 						idConst INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 						idRecette INT(6) UNSIGNED,
 						idAliment INT(6) UNSIGNED,
-						FOREIGN KEY (idAliment) REFERENCES Aliments(idAliment) ON DELETE CASCADE,
-						FOREIGN KEY (idRecette) REFERENCES Recettes(idRecette) ON DELETE CASCADE
+						FOREIGN KEY (idAliment) REFERENCES Aliments(idAliment) ON UPDATE CASCADE,
+						FOREIGN KEY (idRecette) REFERENCES Recettes(idRecette) ON UPDATE CASCADE
 					)';
 
 		foreach (explode(';',$creation) as $requete)
@@ -50,19 +50,20 @@
 		die('Erreur : ' . $e->getMessage());
 	}
 
+
 	/* =============== Insertion des données =============== */
 	echo '<br />Insertion des données<br />';
 
-	include('Donnees.inc.php'); // On inclut le script PHP qui contient les tableaux $Recettes et $Hiérarchie
+	include('Donnees.inc.php'); // On inclut le script PHP qui contient les tableaux $Recettes et $Hierarchie
 	
-	if(!empty($Recettes)) // Si tableau $Recettes non vide et non nul
+	if(!empty($Recettes) && !empty($Hierarchie)) // Si tableaux $Recettes et $Hierarchie non vides et non nuls
 	{
 		/* ======== Insertion des recettes ======== */
 		foreach($Recettes as $cocktail)
 		{
 			try
 			{
-				$insertionRecettes = 'INSERT INTO Recettes (titre, composition, preparation) VALUES (:titre, :composition, :preparation)';
+				$insertionRecettes = 'INSERT INTO Recettes (titre, composition, preparation) VALUES (:titre, :composition, :preparation);';
 				$insertionRecettesRequete = $bdd->prepare($insertionRecettes);
 				$insertionRecettesRequete->execute(array('titre' => $cocktail['titre'],
 														'composition' => $cocktail['ingredients'],
@@ -73,7 +74,6 @@
 			{
 				die('Erreur : ' . $pdoErr->getMessage());
 			}
-			
 		}
 		echo '=> Table recettes remplie<br />';
 
@@ -82,7 +82,7 @@
 		{
 			try
 			{
-				$insertionAliments = 'INSERT INTO Aliments (nomAliment) VALUES (:nomAliment)';
+				$insertionAliments = 'INSERT INTO Aliments (nomAliment) VALUES (:nomAliment);';
 				$insertionAlimentsRequete = $bdd->prepare($insertionAliments);
 				$insertionAlimentsRequete->execute(array('nomAliment' => $nomAliment));
 				$insertionAlimentsRequete->closeCursor();
@@ -113,7 +113,7 @@
 											FROM
 											(SELECT idAliment, nomAliment
 												FROM Aliments
-												WHERE nomAliment = :nomSC) AS aliment_superCat))'; // Ne pas oublier de mettre un alias à la table créée par une sous-requête depuis un FROM
+												WHERE nomAliment = :nomSC) AS aliment_superCat));'; // Ne pas oublier de mettre un alias à la table créée par une sous-requête depuis un FROM
 					$insertionSupCatRequete = $bdd->prepare($insertionSupCat);
 
 					/* Exécution de la requête */
@@ -135,10 +135,40 @@
 			}
 		}
 		echo '=> Table Supercategories remplie<br />';
+
+		/* ======== Insertion de la table Constitution, qui fait le lien entre les recettes et leurs ingrédients ======== */
+		foreach($Recettes as $cocktail) // Parcours de toutes les recettes
+		{
+			try
+			{
+				/* Préparation de la requête */
+				$insertionConstitution = 'INSERT INTO Constitution (idRecette, idAliment)
+											VALUES (
+												(SELECT idRecette
+													FROM Recettes
+													WHERE titre = :nomRecette),
+												(SELECT idAliment
+													FROM Aliments
+													WHERE nomAliment = :nomAliment));';
+				$insertionConstitutionRequete = $bdd->prepare($insertionConstitution);
+
+				/* Exécution de la requête */
+				foreach($cocktail['index'] as $nomIndex) // Parcours de tous les ingrédients qui composent les recettes
+				{
+					$insertionConstitutionRequete->execute(array('nomRecette' => $cocktail['titre'], 'nomAliment' => $nomIndex));
+					$insertionConstitutionRequete->closeCursor();
+				}
+			}
+			catch(PDOException $pdoErr)
+			{
+				die('Erreur : ' . $pdoErr->getMessage());
+			}
+		}
 	}
 	else
 	{
+		echo '$Recettes et/ou $Hierarchie n\'existe pas ou est vide<br />';
 		echo print_r($Recettes[0]);
-		echo '$Recettes n\'existe pas ou est vide<br />';
+		echo print_r($Hierarchie[0]);
 	}
 ?>
