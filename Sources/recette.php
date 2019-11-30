a99990cc8ab8140fd4973e0e7fbe26bc20e6b800
Digiskyy <a-blaise@orange.fr>
2019-11-30 17:52:52 +0100

    Ajout page de recette

---
+++ Sources/recette.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Cette page affichera tous les éléments constituant la recette demandée par la méthode GET de la recherche
+ */
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>WeDrink | Recette : </title><!-- TO DO : Ajouter en php le titre de la recette -->
+    <link rel="Stylesheet" href="Style/styleRecette.css" />
+</head>
+
+<body>
+    <!-- EN-TETE -->
+    <header>
+        <!-- TO DO : Mettre logo du site, menu : mode connecté sinon connexion, inscription -->
+    </header>
+
+    <!-- CONTENU -->
+    <section>
+        <h1>Tequila Sunrise</h1><!--TO DO Mettre titre de la recette -->
+
+        <!-- TO DO : Afficher photo du cocktail s'il y en a -->
+
+        <!-- RECETTE -->
+        <article>
+            <div id=composition>
+                <h2>Composition</h2>
+                <ul id="listeIngredients">
+                    <li>10 cL de jus de betterave</li>
+                    <li>1 L de tequila</li>
+                    <li>du soleil</li>
+                </ul>
+            </div>
+
+            <div id="preparation">
+                <h2>Préparation</h2>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore aliquid architecto deleniti quibusdam fugiat quo veniam voluptas necessitatibus praesentium aliquam quisquam consequatur quod qui repellendus reiciendis, velit et, obcaecati quasi?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quasi dicta fugit labore ipsa magnam autem vel nostrum dolorem? Assumenda dolore cumque sequi. Facere quod tempora tempore sint ipsum suscipit.
+                </p>
+            </div>
+        </article>
+
+        <!-- Aside à voir si on met en fixe le titre, une photo en petit si y'a et la liste des ingrédients -->
+    </section>
+
+    <!-- PIED DE PAGE -->
+    <footer>
+
+    </footer>
+</body>
+</html>
