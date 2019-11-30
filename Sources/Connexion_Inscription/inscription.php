bb452a9fe455757d08c75a23683f9ebb63f7b4b8
Digiskyy <a-blaise@orange.fr>
2019-11-30 03:16:01 +0100

    Validation du formulaire

--- Sources/Connexion_Inscription/inscription.php
+++ Sources/Connexion_Inscription/inscription.php
@@ -18,51 +18,51 @@
             <p id=indication>Les champs <em>Pseudo</em> et <em>Mot de passe</em> sont obligatoires.<br />Nous vous informons que les mineurs ne peuvent pas créer de compte.</p>
             <p>
                 <label for="pseudo">Pseudo</label>
-                <input type="text" name="pseudo" id="pseudo" class="inputEcrivable" autofocus required />
+                <input type="text" name="pseudo" id="pseudo" class="inputEcrivable" pattern="^[A-Za-z0-9_éèàùâêôîâ']{2,}$" title="Pseudo : 2 lettres ou chiffres minimum, _ autorisé" maxlength="20" autofocus required /> <!-- pattern permet de mettre une regex que doit respecter l'utilisateur pour que le champ soit valide (ici, pseudo de 2 lettres ou chiffres minimum, _ autorisé) -->
             </p>
             <p>
                 <label for="mdp">Mot de passe</label>
-                <input type="password" name="mdp" id="mdp" class="inputEcrivable" required/>
+                <input type="password" name="mdp" id="mdp" class="inputEcrivable" pattern="^.{2,}$" required/>
             </p>
             <p>
                 <label for="mdpConf">Confirmation du mot de passe</label>
-                <input type="password" name="mdpConf" id="mdpConf" class="inputEcrivable" required/>
+                <input type="password" name="mdpConf" id="mdpConf" class="inputEcrivable" pattern="^.{2,}$" required/>
             </p>
             <fieldset>
                 <legend>Apprenons à se connaître</legend>
                 <p>Votre sexe
                     <div class="radioBouton">
-                        <label for="femme">Femme</label><input type="radio" name="sexe" value="F" id="femme" />
-                        <label for="homme">Homme</label><input type="radio" name="sexe" value="H" id="homme" />
+                        <span class="label_radioBouton"><label for="femme">Femme</label><input type="radio" name="sexe" value="F" id="femme" /></span>
+                        <span class="label_radioBouton"><label for="homme">Homme</label><input type="radio" name="sexe" value="H" id="homme" /></span>
                     </div>
                 </p>
                 <p>
                     <label for="prenom">Prénom</label>
-                    <input type="text" name="prenom" id="prenom" class="inputEcrivable" />
+                    <input type="text" name="prenom" id="prenom" class="inputEcrivable" pattern="^[A-Za-z-]{2,}$" />
                 </p>
                 <p>
                     <label for="nom">Nom</label>
-                    <input type="text" name="nom" id="nom" class="inputEcrivable" />
+                    <input type="text" name="nom" id="nom" class="inputEcrivable" pattern="[A-Za-z-]{2,}" />
                 </p>
                 <p>
                     <label for="age">Age</label>
-                    <input type="number" name="age" id="age" class="inputEcrivable" min="0" />
+                    <input type="number" name="age" id="age" class="inputEcrivable" min="0" pattern="^[1-9]{2}$" />
                 </p>
                 <p>
                     <label for="email">Email</label>
-                    <input type="email" name="email" id="email" class="inputEcrivable" />
+                    <input type="email" name="email" id="email" class="inputEcrivable" pattern="^[a-z0-9éèàùêîôâ_.-]+@[a-z0-9_.-]{2,}\.[a-z]{2,}$" />
                 </p>
                 <p>
                     <label for="tel">Téléphone</label>
-                    <input type="tel" name="tel" id="tel" class="inputEcrivable" />
+                    <input type="tel" name="tel" id="tel" class="inputEcrivable" pattern="^0[1-8]([ .-]?[0-9]{2}){4}$" />
                 </p>
                 <p>
                     <label for="adresse">Adresse</label>
                     <input type="text" name="adresse" id="adresse" class="inputEcrivable" />
                     <label for="codePostal">Code postal</label>
-                    <input type="number" name="codePostal" id="codePostal" class="inputEcrivable" />
+                    <input type="number" name="codePostal" id="codePostal" class="inputEcrivable" pattern="^[1-9]{2}[ ]?[1-9]{4}$" />
                     <label for="ville">Ville</label>
-                    <input type="text" name="ville" id="ville" class="inputEcrivable" />
+                    <input type="text" name="ville" id="ville" class="inputEcrivable" pattern="^[a-zA-Z]+$" />
                 </p>
             </fieldset>
             <p><input type="submit"  value="S'inscrire" /></p>
--- Sources/Connexion_Inscription/inscription_post.php
+++ Sources/Connexion_Inscription/inscription_post.php
@@ -1,5 +1,5 @@
 <?php
-/* Page qui permet d'enregistrer les informations de l'utilisateur dans la base de données et qui redirigera vers la page de connexion */
+/* Page qui permet de créer un compte pour un utilisateur en enregistrant ses informations dans la base de données et qui le redirigera vers la page de connexion */
 
 if(isset($_POST['pseudo']) 
     AND isset($_POST['mdp']) 
@@ -7,55 +7,96 @@ if(isset($_POST['pseudo'])
 {
     //echo 'pseudo : ' . $_POST['pseudo'] . ' | mdp : ' . $_POST['mdp'] . ' | mdpConf : ' . $_POST['mdpConf'] . ' | age : ' . $_POST['age'] . '<br />';
 
-    $mdp = $_POST['mdp'];
-    $mdpConf = $_POST['mdpConf'];
-    if(strcmp($mdp, $mdpConf) == 0) // Si les mots de passe correspondent (comparaison sensible à la casse)
+    $pseudo = securise_chaine($_POST['pseudo']);
+    $mdp = securise_chaine($_POST['mdp']);
+    $mdpConf = securise_chaine($_POST['mdpConf']);
+
+    // TO DO : Quand il y a des erreurs lors de la validation du formulaire, recharger la page et afficher les messages correspondants
+
+    if(strlen($pseudo) > 1 // Pseudo doit avoir minimum 2 lettres
+        AND strlen($mdp) > 1) // Mot de passe minimum 2 lettres
     {
-        /* Envoi des requêtes dans la base de données */
-        try
+        if(strcmp($mdp, $mdpConf) == 0) // Si les mots de passe correspondent (comparaison sensible à la casse)
         {
-            $bdd = new PDO('mysql:host=localhost;dbname=projet_boissons;charset=utf8;', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            /* Envoi des requêtes dans la base de données */
+            try
+            {
+                $bdd = new PDO('mysql:host=localhost;dbname=projet_boissons;charset=utf8;', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
-            $insertionUser = 'INSERT INTO Utilisateurs(pseudo, mdp, sexe, prenom, nom, age, email, telephone, adresse, codePostal, ville, dateCreation) 
-                                VALUES (:pseudo, :mdp, :sexe, :prenom, :nom, :age, :email, :tel, :adresse, :codePostal, :ville, NOW());';
-            $insertionUserRequete = $bdd->prepare($insertionUser);
+                $insertionUser = 'INSERT INTO Utilisateurs(pseudo, mdp, sexe, prenom, nom, age, email, telephone, adresse, codePostal, ville, dateCreation) 
+                                    VALUES (:pseudo, :mdp, :sexe, :prenom, :nom, :age, :email, :tel, :adresse, :codePostal, :ville, NOW());';
+                $insertionUserRequete = $bdd->prepare($insertionUser);
 
-            /* Insertion des données utilisateurs en fonction de leur remplissement ou pas */
-            $donnees = array('pseudo' => $_POST['pseudo'], 'mdp' => $mdp);
-            remplirTableau($donnees, 'sexe');
-            remplirTableau($donnees, 'prenom');
-            remplirTableau($donnees, 'nom');
-            remplirTableau($donnees, 'age');
-            remplirTableau($donnees, 'email');
-            remplirTableau($donnees, 'tel');
-            remplirTableau($donnees, 'adresse');
-            remplirTableau($donnees, 'codePostal');
-            remplirTableau($donnees, 'ville');
+                /* Insertion des données utilisateurs en fonction de leur remplissement ou pas */
+                $donnees = array('pseudo' => $_POST['pseudo'], 'mdp' => $mdp);
+                remplir_tableau($donnees, 'sexe');
+                remplir_tableau($donnees, 'prenom');
+                remplir_tableau($donnees, 'nom');
+                remplir_tableau($donnees, 'age');
+                remplir_tableau($donnees, 'email');
+                remplir_tableau($donnees, 'tel');
+                remplir_tableau($donnees, 'adresse');
+                remplir_tableau($donnees, 'codePostal');
+                remplir_tableau($donnees, 'ville');
 
-            $insertionUserRequete->execute($donnees);
-            $insertionUserRequete->closeCursor();
+                $insertionUserRequete->execute($donnees);
+                $insertionUserRequete->closeCursor();
+            }
+            catch(PDOException $e)
+            {
+                die('Erreur : ' . $e->getMessage());
+            }
+
+            /* Redirection vers la page de connexion */
+            header('Location: connexion.php'); // TO DO : Afficher un message sur la page de connexion informant que le compte est bien enregistré
         }
-        catch(PDOException $e)
+        else
         {
-            die('Erreur : ' . $e->getMessage());
-        }
+            // echo 'Les deux mots de passe ne correspondent pas.<br />';
 
-        /* Redirection vers la page de connexion */
-        header('Location: connexion.php'); // TO DO : Ecrire une ligne sur la page de connexion comme quoi le compte est bien enregistré
+            /* Redirection vers la page d'inscription */
+            header('Location: inscription.php'); // TO DO : Afficher un message expliquant l'erreur
+        }
     }
     else
     {
-        echo 'Les deux mots de passe ne correspondent pas.<br />'; // TO DO : Recharger la page
+        // echo 'Le pseudo et le mot de passe doivent contenir au minimum 2 lettres.<br />';
+
+        /* Redirection vers la page d'inscription */
+        header('Location: inscription.php'); // TO DO : Afficher un message expliquant l'erreur
     }
 }
 else
-    echo 'Une des valeurs du formulaire n\'existe pas.<br />'; // TO DO : Faire une page d'erreur
-
-
-function remplirTableau(&$tableau, $cle) // Passage du tableau par référence
 {
-    if(isset($_POST[$cle]))
-        $tableau[$cle] = $_POST[$cle]; // Rajoute une valeur dans le tableau avec sa clé sans effacer le tableau
+    //echo 'Le pseudo, le mot de passe et sa confirmation sont à remplir obligatoirement.<br />';
+
+    /* Redirection vers la page d'inscription */
+    header('Location: inscription.php'); // TO DO : Afficher un message expliquant l'erreur
+}
+
+
+
+
+/**
+ * Retourne la chaîne sécurisée en minuscule correspondante à celle passé en paramètre
+ */
+function securise_chaine($chaine)
+{
+    $chaineSecurisee = trim($chaine); // trim : supprime les espaces avant et après
+    $chaineSecurisee = strtolower($chaineSecurisee); // strtolower : met en minuscule
+    $chaineSecurisee = stripslashes($chaineSecurisee); // stripslashes : enlève les \
+    $chaineSecurisee = htmlspecialchars($chaineSecurisee); // htmlspecialchars : enlève le rsique d'exécution de script JavaScript ou HTML en transformant les < en &lt
+    return $chaineSecurisee;
+}
+
+
+/**
+ * Remplit le tableau associatif passé par référence en utilisant la même clé pour récupérer la valeur dans le tableau $_POST
+ */
+function remplir_tableau(&$tableau, $cle) // Passage du tableau par référence
+{
+    if(isset($_POST[$cle]) AND !empty($_POST[$cle]))
+        $tableau[$cle] = securise_chaine($_POST[$cle]); // Rajoute une valeur dans le tableau avec sa clé sans effacer le tableau
     else
         $tableau[$cle] = NULL;
 }
